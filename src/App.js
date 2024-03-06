004b6f963b954c76fd491e49dda06a8c783b8a27
gyduddl <gyduddl@naver.com>
2024-03-07 05:19:37 +0900

    스타일드컴포넌트1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { StyleSheet, View, TextInput} from 'react-native';
+// import { StyleSheet, View, TextInput} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import Shadow from './Shadow';
+import styled,{css } from 'styled-components/native';
+
+// View컴포넌트를 실제로 사용한 것이 아니기에 View를 import할 필요 없다.
+const Container = styled.View`
+    flex: 1;
+    background-color: #e3e3e3;
+    align-items: center;
+    justify-content: center;
+`;
+
+const cssText =css`
+  font-size:20px;
+  font-weight: 600;
+`
+const StyledText= styled.Text`
+  ${cssText}
+  color:blue;
+`;
+const ErrorText = styled(StyledText)`
+  color:red
+`;
+const StyledButton = styled.Button``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <StatusBar style='auto'/>
-    <Shadow/>
-    </View>
+      <StyledText>styled components</StyledText>
+      <ErrorText>Error !</ErrorText>
+      <StyledButton title="Styled" onPress={()=>alert('haha')}/>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
