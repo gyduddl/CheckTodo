eff49c1fa3b2e824723188b08d3056eb72b54661
gyduddl <gyduddl@naver.com>
2024-03-09 23:24:32 +0900

    0309

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import React,{useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import styled,{ThemeProvider} from 'styled-components/native';
-import { Switch } from 'react-native';
-import Input2 from './Input2';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
+import Input from './components/input';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
     flex: 1;
-    background-color: ${({theme})=>theme.bgColor};
+    background-color: ${({ theme }) => theme.background};
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
+`;
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.main};
+    width: 100%;
+    align-items: flex-end;
+    padding: 0 20px;
 `;
 
-//라이트 테마
-const lightTheme = {
-  inputColor:'#111111',
-  inputBoder: "#111111",
-  bgColor: "#e3e3e3"
-}
-
-//다크 테마
-const darkTheme = {
-  inputColor: '#e3e3e3',
-  inputBoder: "#e3e3e3",
-  bgColor: "#111111"
-}
-
 export default function App() {
-  const [isLight, toggleTheme] = useState(true);
-  return (
-    <ThemeProvider theme={isLight?darkTheme:lightTheme}>
-    <Container>
-      <StatusBar style='auto'/>
-      <Switch value={isLight} onValueChange={isLight=>toggleTheme(isLight)}/>
-      <Input2 placeholder="Type a message..."/>
-    </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <StatusBar barStyle='light-content' backgroundColor={theme.background} />
+                <Title>TODO List</Title>
+                <Input placeholder='+ Add a Task' />
+            </Container>
+        </ThemeProvider>
+    );
 }
---
+++ src/components/input.js
@@ -0,0 +1,35 @@
+import react from 'react';
+import styled from 'styled-components/native';
+import { Dimensions, useWindowDimensions } from 'react-native';
+
+const StyledInput = styled.TextInput.attrs(({ theme }) => ({
+    placeholderTextColor: theme.main,
+}))`
+    width: ${({ width }) => width - 40}px;
+    height: 60px;
+    margin: 3px 0;
+    padding: 15px 20px;
+    border-radius: 25px;
+    font-size: 25px;
+    background-color: ${({ theme }) => theme.itemBackground};
+    color: ${({ theme }) => theme.text};
+`;
+
+const Input = ({ placeholder }) => {
+    // const width = Dimensions.get('window').width;
+    const width = useWindowDimensions().width;
+    // maxLength=> 최대 50의 길이만 받을 수 있다.
+    return (
+        <StyledInput
+            width={width}
+            placeholder={placeholder}
+            maxLength={50}
+            autoCapitalize='none'
+            autoCorrect={false}
+            returnKeyType='done'
+            keyboardAppearance='dark'
+        />
+    );
+};
+
+export default Input;
