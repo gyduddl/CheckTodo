4a2a5bf4c70e521544c04fab265f6d60c99d18cc
gyduddl <gyduddl4889@naver.com>
2024-04-01 06:03:11 +0900

    Design/ChannelCreation/#27

--- src/screens/ChannelList.js
+++ src/screens/ChannelList.js
@@ -17,7 +17,6 @@ const ChannelList = ({ navigation }) => {
     return (
         <Container>
             <StyledText>Channel List</StyledText>
-            <Button title='Create' onPress={() => navigation.navigate('ChannelCreation')} />
             <Button title='Channel' onPress={() => navigation.navigate('Channel')} />
         </Container>
     );
